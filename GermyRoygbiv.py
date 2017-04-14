b22b03431dfb723b88fbfe4dc700d2f248ce854e
Jeremy Cheung <jeremyccheung@yahoo.com>
2017-04-14 12:13:03 -0500

    Add files via upload

---
+++ GermyRoygbiv.py
@@ -0,0 +1,4 @@
+#This is just a silly test program to play with Definite loops
+for color in ["red","orange","yellow","green","blue","indigo","violet"]:
+    print color
+print "Are the colors that make a rainbow"
